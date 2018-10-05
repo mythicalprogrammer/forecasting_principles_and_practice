5766aaa02ac788e2c5d2b609e5015468728e995b
Anthony Doan <mythicalprogrammer@gmail.com>
2018-10-04 17:55:43 -0700

    chp 2 section 2, finished time plots portion of chapter 2.

---
+++ chapter_2/2_2_time_plots.R
@@ -0,0 +1,9 @@
+autoplot(melsyd[, "Economy.Class"]) +
+  ggtitle("Economy class passengers: Melbourne-Sydney") +
+  xlab("Year") +
+  ylab("Thousands")
+
+autoplot(a10) +
+  ggtitle("Antidiabetic drug sales") +
+  ylab("$ million") +
+  xlab("Year")
